61f7ae6053c9d7034e56170025c4c63baa71deb4
VincentQu <v.quantmeyer@gmail.com>
2023-11-17 11:49:09 +0100

    Data folder structure

--- scripts/sh/download_valse.sh
+++ scripts/sh/download_valse.sh
@@ -3,8 +3,8 @@
 IMG_URL="http://vision.stanford.edu/yukezhu/visual7w_images.zip"
 VALSE_URL="https://raw.githubusercontent.com/Heidelberg-NLP/VALSE/main/data/existence.json"
 
-IMG_FOLDER_PATH="../data/img/visual7w"
-VALSE_FOLDER_PATH="../data/valse"
+IMG_FOLDER_PATH="../../data/raw/img/visual7w"
+VALSE_FOLDER_PATH="../../data/raw/valse"
 VALSE_DESTINATION_DIRECTORY="$VALSE_FOLDER_PATH/valse_existence.json"
 
 wget $IMG_URL -O v7w_images.zip

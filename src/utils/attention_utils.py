44467bc3e861b143f8a7470a9e2b3f7277d59a7f
VincentQu <v.quantmeyer@gmail.com>
2024-03-15 17:09:30 +0100

    Update utils

--- src/utils/attention_utils.py
+++ src/utils/attention_utils.py
@@ -1,4 +1,31 @@
-def generate_ablation_chart_filepath(config):
+import h5py
+
+def generate_attention_chart_filepath(config):
     filename = 'attention_' + '_'.join([str(v) for v in config.values()]) + '.png'
     filepath = f"../../output/charts/attention/{filename}"
     return filepath
+
+def generate_attention_result_filepath(config):
+    filename = 'attention_' + '_'.join([str(v) for v in config.values()]) + '.h5'
+    filepath = f"../../data/experiments/attention/{filename}"
+    return filepath
+
+def store_attention_results(results, correlations, config=None):
+    valid_encoders = ['vision', 'text']
+    assert config['encoder'] in valid_encoders, f"'encoder' in config must be one of {valid_encoders}"
+
+    h5_filepath = generate_attention_result_filepath(config)
+
+    with h5py.File(h5_filepath, 'w') as hdf:
+
+        hdf.create_dataset(name='results', data=results)
+
+        if correlations is not None:
+            hdf.create_dataset(name='correlations', data=correlations)
+
+        if config is not None:
+            grp_config = hdf.create_group('config')
+            for key, value in config.items():
+                grp_config.attrs[key] = str(value)
+
+    print(f'Results saved to: {h5_filepath}')
--- src/utils/causal_tracing_utils.py
+++ src/utils/causal_tracing_utils.py
@@ -1,3 +1,5 @@
+import h5py
+
 def create_tracing_hook_fn(layer, batch_idx, position, activations):
     def hook_fn(module, input, output):
         if layer == 0:  # embeddings
@@ -19,3 +21,25 @@ def generate_tracing_chart_filepath(config):
     filename = 'causal_tracing_' + '_'.join([str(v) for v in config.values()]) + '.png'
     filepath = f"../../output/charts/causal_tracing/{filename}"
     return filepath
+
+def generate_tracing_result_filepath(config):
+    filename = 'causal_tracing_' + '_'.join([str(v) for v in config.values()]) + '.h5'
+    filepath = f"../../data/experiments/causal_tracing/{filename}"
+    return filepath
+
+def store_causal_tracing_results(results, config=None):
+    valid_encoders = ['vision', 'text']
+    assert config['encoder'] in valid_encoders, f"'encoder' in config must be one of {valid_encoders}"
+
+    h5_filepath = generate_tracing_result_filepath(config)
+
+    with h5py.File(h5_filepath, 'w') as hdf:
+
+        hdf.create_dataset(name='results', data=results)
+
+        if config is not None:
+            grp_config = hdf.create_group('config')
+            for key, value in config.items():
+                grp_config.attrs[key] = str(value)
+
+    print(f'Results saved to: {h5_filepath}')
